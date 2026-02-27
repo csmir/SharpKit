0db4eea2dea6120779dabe928a4c449bce75e49f
Red-K0 <114494949+Red-K0@users.noreply.github.com>
2026-02-27 20:06:35 +0200

    Document ReflectionExtensions.cs

--- src/SharpKit/Extensions/Reflection/ReflectionExtensions.cs
+++ src/SharpKit/Extensions/Reflection/ReflectionExtensions.cs
@@ -1,7 +1,18 @@
 ﻿namespace SharpKit;
 
+/// <summary>
+///     Provides useful extensions for supported reflection contexts.
+/// </summary>
 public static class ReflectionExtensions
 {
-    public static IEnumerable<Attribute> GetAttributes(this ICustomAttributeProvider attributeProvider, bool inherit = false)
-        => attributeProvider.GetCustomAttributes(inherit).OfType<Attribute>();
+    extension(ICustomAttributeProvider attributeProvider)
+    {
+        /// <summary>
+        ///     Returns a list of all attributes defined on this provider, excluding named attributes, in a strongly typed manner.
+        /// </summary>
+        /// <param name="inherit"> When true, look up the hierachy chain for inherited attributes as well. </param>
+        /// <returns> An <see cref="IEnumerable{T}"/> of attributes, or an empty array. </returns>
+        /// <exception cref="TypeLoadException"/>
+        public IEnumerable<Attribute> GetAttributes(bool inherit = false) => attributeProvider.GetCustomAttributes(inherit).OfType<Attribute>();
+    }
 }
